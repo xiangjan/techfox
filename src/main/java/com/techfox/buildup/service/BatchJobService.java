968af01900b19d83d80035c94a734e44b2e6a4a8
sanggen <xjw0118@hotmail.com>
2023-01-16 22:50:31 +0900

    enqueueJob method.

---
+++ src/main/java/com/techfox/buildup/service/BatchJobService.java
@@ -0,0 +1,29 @@
+package com.techfox.buildup.service;
+
+import org.jobrunr.jobs.JobId;
+import org.jobrunr.jobs.context.JobContext;
+import org.jobrunr.scheduling.JobScheduler;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BatchJobService {
+
+    final private JobScheduler jobScheduler;
+
+    public BatchJobService(JobScheduler jobScheduler) {
+        this.jobScheduler = jobScheduler;
+    }
+
+    public JobId enqueueJob() {
+
+        return jobScheduler.enqueue(UUID.randomUUID(),
+                () -> runScheduledJob("name", JobContext.Null)
+        );
+    }
+
+    public void runScheduledJob(String id, JobContext jobContext) {
+
+    }
+}
