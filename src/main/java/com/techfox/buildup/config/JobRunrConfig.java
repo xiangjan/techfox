bd12c73842e89f19b75d053cb216134bb5e030ee
sanggen <xjw0118@hotmail.com>
2023-01-16 22:18:51 +0900

    Init commit

---
+++ src/main/java/com/techfox/buildup/config/JobRunrConfig.java
@@ -0,0 +1,18 @@
+package com.techfox.buildup.config;
+
+import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobRunrConfig {
+
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper){
+        StorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(jobMapper);
+        return storageProvider;
+    }
+}
---
+++ src/test/java/com/techfox/buildup/BuildupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfox.buildup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
